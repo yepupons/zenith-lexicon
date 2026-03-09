8a49c8ed3077b8a1d2ceade988b6fd10e3727325
Фетюков Никита <nikfet24@gmail.com>
2026-03-10 00:53:28 +0300

    chore: update for new naming

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/zenith-lexicon/",
+  base: "/zercalo/",
   server: {
     host: "::",
     port: 8080,
@@ -13,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
